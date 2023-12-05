fb2e3229e159e93ac3f8781791820e0a1092d1e4
iflorain <150113291+iflorain@users.noreply.github.com>
2023-12-05 17:17:27 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,39 @@
 import streamlit as st
-from streamlit import secrets
+import openai
+# Uncomment the following lines to enable the API key input form
+# Initialize
+st.cache_data.clear()
 
-openai.api_key = secrets["openai_api_key"]
+if "openai_api_key" not in st.session_state:
+    st.session_state.openai_api_key = ""
 
+openai.api_key = st.session_state.openai_api_key
 
-def generate_cuisine_recommendation(cuisine, meal_type, flavor_preferred):
+if "text_error" not in st.session_state:
+    st.session_state.text_error = None
+
+if "text" not in st.session_state:
+    st.session_state.text = None
+
+if "n_requests" not in st.session_state:
+    st.session_state.n_requests = 0
+
+with st.sidebar:
+    api_key_form = st.form(key="api_key_form")
+    openai_api_key = api_key_form.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    api_key_form_submitted = api_key_form.form_submit_button("Submit")
+
+    if api_key_form_submitted:
+        st.session_state.openai_api_key = openai_api_key
+        openai.api_key = st.session_state.openai_api_key
+        st.success("Your OpenAI API key was saved successfully!")
+
+#user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
+#client = openai.OpenAI(api_key=user_api_key)
+
+def generate_flower_recommendation(occasion, recipient_name, favorite_color, relationship):
     # Customize the prompt based on your requirements
-    prompt = f"I feel like having {meal_type} {cuisine} food with a {flavor_preferred} flavor. What dish do you recommend?. and write 3 notes for me why I chose this cuisine for this {meal_type}."
+    prompt = f"Recommend me a flower that are suitable for {occasion} and {favorite_color} for {recipient_name} who is my {relationship}. and write 5 notes for me to tell {recipient_name} why I chose this flower for this {occasion}."
 
     # Call OpenAI API for recommendation
     response = openai.chat.completions.create(
@@ -15,44 +42,32 @@ def generate_cuisine_recommendation(cuisine, meal_type, flavor_preferred):
         top_p=0.7,
         max_tokens=450,
         messages=[
-            {"role": "system", "content": "You are a cuisine recommendation bot. You will help users find the best dishes for their meal."},
-            {"role": "user", "content": f"You will help users find the best dishes and make notes from the context:{prompt}."},
+            {"role": "system", "content": "You are a flowers recommendation bot. You will help users find the best flowers for their important person."},
+            {"role": "user", "content": f"You will help users find the best flowers and make notes from the context:{prompt}."},
         ]
     )
     
     return response.choices[0].message.content
 
-#st.title("Dish For Today")
-st.markdown("<h2 style = 'font-size: 1.8rem'>Dish For Today</h2>",unsafe_allow_html=True)
+#st.title("🌼Flower For Your Important Person🌼")
+st.markdown("<h2 style = 'font-size: 1.8rem'>🌼Flower For Your Important Person🌼</h2>",unsafe_allow_html=True)
 
 # Uncomment the following lines to enable the API key input form
 
-def main():
-    st.title("Dish For Today")
 
-    # Ask for OpenAI API key as a password
-    api_key = st.text_input("Enter OpenAI API Key:", type="password")
-
-    if st.button("Submit"):
-        # Check if the API key is correct (you may want to implement a more secure validation)
-        if api_key == secrets["openai_api_key"]:
-            st.success("API Key Verified! You have access to the app.")
-            # User input
-            meal_type = st.text_input("Meal Type:")
-            cuisune = st.text_input("Cuisine:")
-            flavor_preferred = st.text_input("Flavor:")
+# User input
+occasion = st.text_input("Occasion:")
+recipient_name = st.text_input("Recipient's Name:")
+favorite_color = st.text_input("Recipient's Favorite Color:")
+relationship = st.text_input("Recipient's Relationship to you:")
 
 # Generate recommendation
-            if st.button("Generate Recommendation"):
-                if meal_type and cuisune and flavor_preferred:
-                    recommendation = generate_cuisine_recommendation(
-                    meal_type, cuisune, flavor_preferred
+if st.button("Generate Recommendation"):
+    if occasion and recipient_name and favorite_color and relationship:
+        recommendation = generate_flower_recommendation(
+            occasion, recipient_name, favorite_color, relationship
         )
-                    st.success(f"Recommended Dish: {recommendation}")
-                else:
-                    st.warning("Please fill in all fields.")
-        else:
-            st.error("Invalid API Key. Please enter the correct API key.")
+        st.success(f"Recommended Flower: {recommendation}")
+    else:
+        st.warning("Please fill in all fields.")
             
-if __name__ == "__main__":
-    main()
