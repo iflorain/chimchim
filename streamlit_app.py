7f1e632c6b2ce9cd43a2cba1c9318980102bcbaf
iflorain <150113291+iflorain@users.noreply.github.com>
2023-12-05 16:05:23 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,39 @@
 import streamlit as st
 import os
+import openai
 
 # Setting the API key
-#openai.api_key = os.environ['OPENAI_API_KEY']
+openai.api_key = os.environ['OPENAI_API_KEY']
 #os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 #import openai
 # Uncomment the following lines to enable the API key input form
 # Initialize
-st.cache_data.clear()
+#st.cache_data.clear()
 
-if "openai_api_key" not in st.session_state:
-    st.session_state.openai_api_key = ""
+#if "openai_api_key" not in st.session_state:
+    #st.session_state.openai_api_key = ""
 
-openai.api_key = st.session_state.openai_api_key
+#openai.api_key = st.session_state.openai_api_key
 #openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-if "text_error" not in st.session_state:
-    st.session_state.text_error = None
+#if "text_error" not in st.session_state:
+    #st.session_state.text_error = None
 
-if "text" not in st.session_state:
-    st.session_state.text = None
+#if "text" not in st.session_state:
+    #st.session_state.text = None
 
-if "n_requests" not in st.session_state:
-    st.session_state.n_requests = 0
+#if "n_requests" not in st.session_state:
+    #st.session_state.n_requests = 0
 
-with st.sidebar:
-    api_key_form = st.form(key="api_key_form")
-    openai_api_key = api_key_form.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    api_key_form_submitted = api_key_form.form_submit_button("Submit")
+#with st.sidebar:
+    #api_key_form = st.form(key="api_key_form")
+    #openai_api_key = api_key_form.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    #api_key_form_submitted = api_key_form.form_submit_button("Submit")
 
-    if api_key_form_submitted:
-        st.session_state.openai_api_key = openai_api_key
-        openai.api_key = st.session_state.openai_api_key
-        st.success("Your OpenAI API key was saved successfully!")
+    #if api_key_form_submitted:
+        #st.session_state.openai_api_key = openai_api_key
+        #openai.api_key = st.session_state.openai_api_key
+        #st.success("Your OpenAI API key was saved successfully!")
 
 #user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 #client = openai.OpenAI(api_key=user_api_key)
