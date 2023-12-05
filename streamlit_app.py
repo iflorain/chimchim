2ab8589b465999acbc634702bc5cb22ed253d524
iflorain <150113291+iflorain@users.noreply.github.com>
2023-12-05 17:27:26 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-#import openai
-from streamlit import secrets
-
-# Set the OpenAI API key using Streamlit secrets
-openai.api_key = secrets["openai_api_key"]
+import openai
 
 # Function to generate a food recommendation using OpenAI
 def generate_food_recommendation(cuisine, meal_type, flavor_preferred):
@@ -29,7 +25,7 @@ def main():
 
     if st.button("Submit"):
         # Check if the API key is correct (you may want to implement a more secure validation)
-        if api_key == openai.api_key:
+        if api_key == "your_secret_api_key":
             st.success("API Key Verified! You have access to the app.")
 
             # User input
