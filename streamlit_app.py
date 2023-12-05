19bfe3fe16a1162153be80b0b66733ad40f59dd8
iflorain <150113291+iflorain@users.noreply.github.com>
2023-12-05 13:07:50 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import openai
+# Uncomment the following lines to enable the API key input form
+# Initialize
+st.cache_data.clear()
+
+if "openai_api_key" not in st.session_state:
+    st.session_state.openai_api_key = ""
+
+openai.api_key = st.session_state.openai_api_key
+
+if "text_error" not in st.session_state:
+    st.session_state.text_error = None
+
+if "text" not in st.session_state:
+    st.session_state.text = None
+
+if "n_requests" not in st.session_state:
+    st.session_state.n_requests = 0
+
+with st.sidebar:
+    api_key_form = st.form(key="api_key_form")
+    openai_api_key = api_key_form.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    api_key_form_submitted = api_key_form.form_submit_button("Submit")
+
+    if api_key_form_submitted:
+        st.session_state.openai_api_key = openai_api_key
+        openai.api_key = st.session_state.openai_api_key
+        st.success("Your OpenAI API key was saved successfully!")
